def48e8d6f44e5442a4de1d483046abf67fc9d6a
Theo Pearson-Bray <tpb1908dev@gmail.com>
2020-05-01 11:52:55 +0100

    Update config to use combined white/blacklist.

--- rofication/_interceptor.py
+++ rofication/_interceptor.py
@@ -1,7 +1,7 @@
 import os
 import re
 from warnings import warn
-from typing import Callable, List, Tuple, Pattern
+from typing import Callable, List, Tuple, Pattern, NewType
 import subprocess
 import threading
 import pyinotify
@@ -48,24 +48,17 @@ Keys correspond to a part of the notification.
 """
 class ConfiguredInterceptor(BaseInterceptor):
 
-    KEYS = ["all", "summary", "body", "application"]
-
 
 
     def __init__(self, config_path='~/.config/regolith/rofications/config'):
         config_path = os.path.expanduser(config_path)
 
         self.config = {}
-        self.whitelist = []
-        self.blacklist = []
         self.matchers = []
-        # TODO: Deal with files that don't exist
 
         def read():
             print(f"Loading config file")
-            self.read_file(config_path)
-
-
+            self.load_config(config_path)
 
         folder = os.path.dirname(os.path.abspath(config_path))
         wm1 = pyinotify.WatchManager()
@@ -75,78 +68,11 @@ class ConfiguredInterceptor(BaseInterceptor):
         wm1.add_watch(folder, pyinotify.IN_CLOSE_WRITE, rec=True, auto_add=True)
 
         read()
-        print(f"Loaded whitelist {self.whitelist}")
-        print(f"Loaded blacklist {self.blacklist}")
+        print(f"Loaded matchers {self.matchers}")
 
+    def load_config(self, path):
+        pass
 
-    def read_file(self, path):
-        with open(path, 'r') as f:
-            mode = ""
-            # TODO: Log errors
-            for i, line in enumerate(f.readlines()):
-                line = line.rstrip()
-
-                if not line.startswith("#"):
-                    if line.startswith("["):
-                        mode = line
-                        continue
-                    if mode == "[config]":
-                        self.parse_config_key(line)
-                    elif mode == "[whitelist]":
-                        self.parse_whitelist(line)
-                    elif line == "[blacklist]":
-                        self.parse_blacklist(line)
-                    else:
-                        warn(f"Unrecognised config mode {mode}")
-
-    def parse_whitelist(self, line) -> bool:
-        matcher = self.parse_matcher(line)
-        if matcher is not None:
-            self.whitelist.append(matcher)
-            return True
-        return False
-
-    def parse_blacklist(self, line) -> bool:
-        matcher = self.parse_matcher(line)
-        if matcher is not None:
-            self.blacklist.append(matcher)
-            return True
-        return False
-
-    def parse_matcher(self, line):
-        splits = line.split(":", 1)
-        # TODO: Support whitespace between key and regex. Or some better format
-        if len(splits) == 2 and splits[0] in self.KEYS:
-            try:
-                return (splits[0], re.compile(splits[1]))
-            except re.error:
-                pass
-        return None
-
-
-    def parse_config_key(self, line) -> bool:
-        splits = line.split("=", 1)
-        if len(splits) == 2:
-            self.config[splits[0]] = splits[1]
-            print(f"Config entry {splits[0]} : {splits[1]}")
-            return True
-        return False
-
-    def matches_any_in(self, notification: Notification, matchers: List[Tuple[str, Pattern]]) -> bool:
-        for k, m in matchers:
-            if ((k == "all" or k == "summary") and m.match(notification.summary)) or \
-                ((k == "all" or k == "body") and m.match(notification.body)) or \
-                ((k == "all" or k == "application") and m.match(notification.application)):
-                return True
-        return False
-
-    def get_config_bool(self, key, default=False):
-        if key in self.config:
-            if self.config[key] in ['true', '1', 't', 'y', 'yes']:
-                return True
-            elif self.config[key] in ['false', '0', 'f', 'n', 'no']:
-                return False
-        return default
 
 #https://stackoverflow.com/a/2581943
 def popen_and_call(on_exit: Callable[[int], None], popen_args):
@@ -170,19 +96,83 @@ def popen_and_call(on_exit: Callable[[int], None], popen_args):
 
 
 
-class NagBarInterceptor(ConfiguredInterceptor):
+class DefaultInterceptor(ConfiguredInterceptor):
 
+    KEYS = ["all", "summary", "body", "application"]
+
+    Matcher = NewType("Matcher", Tuple[str, Pattern, bool])
+
+    def load_config(self, path):
+        if os.path.exists(path):
+            pass
+        if os.path.isfile(path):
+            pass
+        with open(path, 'r') as f:
+            mode = ""
+            # TODO: Log errors
+            for i, line in enumerate(f.readlines()):
+                line = line.rstrip()
+
+                if not line.startswith("#"):
+                    if line.startswith("["):
+                        mode = line
+                        continue
+                    if mode == "[config]":
+                        self.parse_config_key(line)
+                    elif mode == "[list]":
+                        self.parse_matcher(line)
+                    else:
+                        warn(f"Unrecognised config mode {mode}")
+
+    def parse_matcher(self, line):
+        if line[0] == '!':
+            blacklist = True
+            splits = line[:1].split(":", 1)
+        else:
+            blacklist = False
+            splits = line.split(":", 1)
+        # TODO: Support whitespace between key and regex. Or some better format
+        if len(splits) == 2 and splits[0] in self.KEYS:
+            try:
+                self.matchers.append((splits[0], re.compile(splits[1]), blacklist))
+            except re.error:
+                pass
+        return None
+
+
+    def parse_config_key(self, line) -> bool:
+        splits = line.split("=", 1)
+        if len(splits) == 2:
+            self.config[splits[0]] = splits[1]
+            print(f"Config entry {splits[0]} : {splits[1]}")
+            return True
+        return False
+
+    def get_config_bool(self, key, default=False):
+        if key in self.config:
+            if self.config[key] in ['true', '1', 't', 'y', 'yes']:
+                return True
+            elif self.config[key] in ['false', '0', 'f', 'n', 'no']:
+                return False
+        return default
+
+    def matches(self, notification: Notification, matcher: Matcher):
+        k, m, _ = matcher
+        if ((k == "all" or k == "summary") and m.match(notification.summary)) or \
+                ((k == "all" or k == "body") and m.match(notification.body)) or \
+                ((k == "all" or k == "application") and m.match(notification.application)):
+            return True
 
     def intercept(self, notification: Notification, on_viewed: Callable[[bool], None]):
         if notification.urgency == Urgency.CRITICAL and self.get_config_bool("always_display_critical"):
             self.dispatch_nagbar(notification)
             return
 
-        whitelisted = self.matches_any_in(notification, self.whitelist)
-        blacklisted = self.matches_any_in(notification, self.blacklist)
-        # TODO: Order should be configurable
-        if whitelisted and not blacklisted:
-            self.dispatch_nagbar(notification, on_viewed)
+        for m in self.matchers:
+            if self.matches(notification, m):
+                print(f"Notificaiton matched {m}")
+                if m[2]:
+                    self.dispatch_nagbar(notification, on_viewed)
 
 
     def dispatch_nagbar(self, notification: Notification, on_viewed: Callable[[bool], None]):
